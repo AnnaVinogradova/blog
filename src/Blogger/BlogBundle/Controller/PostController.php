3cc62cfc2a1ff9b21adef03eaa54595efd714ded
AnnaVinogradova <ajellyfish14@gmail.com>
2016-08-01 14:22:00 +0300

    Added access control to Post

--- src/Blogger/BlogBundle/Controller/PostController.php
+++ src/Blogger/BlogBundle/Controller/PostController.php
@@ -22,17 +22,31 @@ class PostController extends Controller
     {
         $securityContext = $this->container->get('security.context');
         $em = $this->getDoctrine()->getManager();
+        $user = $securityContext->getToken()->getUser();
 
         if($securityContext->isGranted('ROLE_ADMIN')){
-            $posts = $em->getRepository('BloggerBlogBundle:Post')->findAll();
-        } else {
-            $user = $securityContext->getToken()->getUser();
-            $posts = $em->getRepository('BloggerBlogBundle:Post')->findByAuthor($user->getUsername());
-        }
+            $posts = $em->getRepository('BloggerBlogBundle:Post')->findBy(
+                array(), 
+                array('created' => 'DESC'));
 
+            $myPosts = $em->getRepository('BloggerBlogBundle:Post')->findBy(
+                array('author' => $user), 
+                array('created' => 'DESC'));
+
+            return $this->render('post/index.html.twig', array(
+            'posts' => $posts,
+            'my_posts' => $myPosts
+            ));
+            
+        } else {    
+            $posts = $em->getRepository('BloggerBlogBundle:Post')->findBy(
+                array('author' => $user), 
+                array('created' => 'DESC'));
+        
         return $this->render('post/index.html.twig', array(
             'posts' => $posts,
         ));
+        }
     }
 
     /**
@@ -50,8 +64,6 @@ class PostController extends Controller
 
             $user = $this->container->get('security.context')->getToken()->getUser();
             $user->addPost($post);
-
-            //$em->persist($post);
             $em->flush();
 
             return $this->redirectToRoute('post_show', array('id' => $post->getId()));
@@ -69,6 +81,15 @@ class PostController extends Controller
      */
     public function showAction(Post $post)
     {
+        $securityContext = $this->container->get('security.context');
+        $em = $this->getDoctrine()->getManager();
+
+        if(! $securityContext->isGranted('ROLE_ADMIN')){
+            if(! $this->checkAccess($post->getId())){
+                return $this->render('post/access_denied.html.twig');
+            }
+        }
+
         $deleteForm = $this->createDeleteForm($post);
 
         return $this->render('post/show.html.twig', array(
@@ -83,6 +104,14 @@ class PostController extends Controller
      */
     public function editAction(Request $request, Post $post)
     {
+        $securityContext = $this->container->get('security.context');
+
+        if(! $securityContext->isGranted('ROLE_ADMIN')){
+            if(! $this->checkAccess($post->getId())){
+                return $this->render('post/access_denied.html.twig');
+            }
+        }
+
         $deleteForm = $this->createDeleteForm($post);
         $editForm = $this->createForm('Blogger\BlogBundle\Form\PostType', $post);
         $editForm->handleRequest($request);
@@ -108,6 +137,15 @@ class PostController extends Controller
      */
     public function deleteAction(Request $request, Post $post)
     {
+        $securityContext = $this->container->get('security.context');
+        $em = $this->getDoctrine()->getManager();
+
+        if(! $securityContext->isGranted('ROLE_ADMIN')){
+            if(! $this->checkAccess($post->getId())){
+                return $this->render('post/access_denied.html.twig');
+            }
+        }
+
         $form = $this->createDeleteForm($post);
         $form->handleRequest($request);
 
@@ -118,6 +156,25 @@ class PostController extends Controller
         }
 
         return $this->redirectToRoute('post_index');
+    }
+
+     /**
+     * Read post.
+     *
+     */
+    public function viewAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em->getRepository('BloggerBlogBundle:Post')->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Unable to find required post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Post:show.html.twig', array(
+            'post'      => $post,
+        ));
     }
 
     /**
@@ -136,17 +193,15 @@ class PostController extends Controller
         ;
     }
 
-    public function viewAction($id){
+    private function checkAccess($id)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
         $em = $this->getDoctrine()->getManager();
 
-        $post = $em->getRepository('BloggerBlogBundle:Post')->find($id);
-
-        if (!$post) {
-            throw $this->createNotFoundException('Unable to find required post.');
-        }
-
-        return $this->render('BloggerBlogBundle:Post:show.html.twig', array(
-            'post'      => $post,
-        ));
+        return  $em->getRepository('BloggerBlogBundle:Post')->findBy( 
+                array('id' => $id,
+                'author' => $user)
+                );
     }
+
 }
